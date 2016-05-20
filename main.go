c2c1a5f96643139396d08afb92e033b6bd600d05
Karol Duleba <mr.fuxi@gmail.com>
2016-05-20 22:41:30 +0100

    Wrap digit drawer function. Center the digit

--- main.go
+++ main.go
@@ -11,20 +11,30 @@ func fontFileName(fontData draw2d.FontData) string {
 	return fontData.Name + ".ttf"
 }
 
+func drawDigitsWithFont(digit string, fontName string, fontSize float64) image.Image {
+	canvas := image.NewRGBA(image.Rect(0, 0, 28, 28))
+	gc := draw2dimg.NewGraphicContext(canvas)
+
+	gc.DrawImage(image.White)    // Background color
+	gc.SetFillColor(image.Black) // Text color
+
+	gc.SetFontData(draw2d.FontData{Name: fontName})
+	gc.SetFontSize(fontSize)
+
+	left, top, right, bottom := gc.GetStringBounds(digit)
+	height := bottom - top
+	width := right - left
+
+	center := 28.0 / 2
+	gc.FillStringAt(digit, center-width/2, center+height/2)
+
+	return canvas
+}
+
 func main() {
 	draw2d.SetFontFolder("./fonts/")
 	draw2d.SetFontNamer(fontFileName)
 
-	dest := image.NewRGBA(image.Rect(0, 0, 105, 18))
-	gc := draw2dimg.NewGraphicContext(dest)
-	gc.FillStroke()
-
-	gc.SetFontData(draw2d.FontData{Name: "OpenSans-Regular"})
-	gc.SetFillColor(image.Black) // Text color
-	gc.SetFontSize(14)
-	gc.FillStringAt("0123456789", 0, 14)
-	gc.Close()
-
-	// Save to file
-	draw2dimg.SaveToPngFile("digits.png", dest)
+	digit := drawDigitsWithFont("8", "OpenSans-Regular", 14)
+	draw2dimg.SaveToPngFile("digits.png", digit)
 }
