0746b852356646b768c283fac35c54aae7d04b6c
Karol Duleba <mr.fuxi@gmail.com>
2016-05-24 22:01:45 +0100

    New fonts. Produce images for all fonts in multiple sizes

--- fonts/README.md
+++ fonts/README.md
@@ -1,3 +1,8 @@
 # Sources
 
 * OpenSans-Regular.ttf - https://github.com/FontFaceKit/open-sans
+* 0xA000*.ttf - http://pippin.gimp.org/0xA000/index.html
+
+# Possible sources
+
+* https://fontlibrary.org
--- main.go
+++ main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"fmt"
 	"image"
+	"io/ioutil"
 	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/llgcode/draw2d"
 	"github.com/llgcode/draw2d/draw2dimg"
 )
 
-var outDir = "out"
+var (
+	outDir  = "out"
+	fontDir = "fonts"
+)
 
 func fontFileName(fontData draw2d.FontData) string {
-	return fontData.Name + ".ttf"
+	return fontData.Name
 }
 
-func drawDigitsWithFont(digit string, fontName string, fontSize float64) image.Image {
+func drawDigitsWithFont(char string, fontName string, fontSize float64) image.Image {
 	canvas := image.NewRGBA(image.Rect(0, 0, 28, 28))
 	gc := draw2dimg.NewGraphicContext(canvas)
 
@@ -26,18 +31,18 @@ func drawDigitsWithFont(digit string, fontName string, fontSize float64) image.I
 	gc.SetFontData(draw2d.FontData{Name: fontName})
 	gc.SetFontSize(fontSize)
 
-	left, top, right, bottom := gc.GetStringBounds(digit)
+	left, top, right, bottom := gc.GetStringBounds(char)
 	height := bottom - top
 	width := right - left
 
 	center := 28.0 / 2
-	gc.FillStringAt(digit, center-width/2, center+height/2)
+	gc.FillStringAt(char, center-width/2, center+height/2)
 
 	return canvas
 }
 
 func main() {
-	draw2d.SetFontFolder("./fonts/")
+	draw2d.SetFontFolder(fontDir)
 	draw2d.SetFontNamer(fontFileName)
 
 	os.RemoveAll(outDir)
@@ -46,15 +51,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	text := `123456789 +=\|/[]*-$#@`
-	for i, c := range text {
-		digit := drawDigitsWithFont(string(c), "OpenSans-Regular", 14)
+	fontFiles, err := ioutil.ReadDir(fontDir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-		fileName := fmt.Sprintf("char-%06d.png", i)
-		err := draw2dimg.SaveToPngFile(path.Join(outDir, fileName), digit)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+	text := `123456789 +=\|/[]*-$#@`
+	fontSizes := []float64{10, 14, 16, 18, 20, 22, 24, 26}
+
+	cnt := 0
+	for _, font := range fontFiles {
+		if filepath.Ext(font.Name()) != ".ttf" {
+			continue
+		}
+		for _, c := range text {
+			for _, fontSize := range fontSizes {
+				cnt++
+				digit := drawDigitsWithFont(string(c), font.Name(), fontSize)
+
+				fileName := fmt.Sprintf("char-%06d.png", cnt)
+				err := draw2dimg.SaveToPngFile(path.Join(outDir, fileName), digit)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			}
 		}
 	}
 }
